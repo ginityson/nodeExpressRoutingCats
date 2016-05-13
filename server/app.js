cb50d025f12d45978a5b9ac761ca2d297337c283
ginityson <ginityson@yahoo.com>
2016-05-13 13:44:06 -0500

    updated port

--- server/app.js
+++ server/app.js
@@ -3,23 +3,15 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var index = require('./routes/index');
+var felines = require('./routes/felines');
 
-app.set('port', 5000);
+app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Data
-var cats = ['Kris', 'Luna', 'Rubio'];
 
 //ROUTES
-app.get('/felines', function(req, res) {
-  res.send(cats);
-});
-
-app.post('/felines', function(req, res) {
-  cats.push(req.body.catName);
-  res.send(true);
-});
+app.use('/felines', felines);
 
 app.use('/', index);
 
